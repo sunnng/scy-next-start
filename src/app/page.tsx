1eaae77b5eb7e98e6df27fcf65a2aad90b7786f3
sunnng <61936492+sunnng@users.noreply.github.com>
2025-04-02 23:50:59 +0800

    add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
-	return <div>home</div>;
+	return (
+		<div>
+			<Button variant={"default"}>Home</Button>
+		</div>
+	);
 }
